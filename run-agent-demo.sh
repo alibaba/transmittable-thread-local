d4b4313e96e7287d37ed12e6a7309b170918f333
Jerry Lee <oldratlee@gmail.com>
2013-10-14 10:09:01 +0800

    update run-agent-demo.sh

--- run-agent-demo.sh
+++ run-agent-demo.sh
@@ -10,10 +10,11 @@ runCmd() {
 
 mvn clean install -Dmaven.test.skip && mvn test-compile &&
 mvn dependency:copy-dependencies -DincludeScope=provided &&
+mvn dependency:copy-dependencies -DincludeScope=runtime &&
 cd target && {
     classpath=`echo dependency/*.jar | tr ' ' :`
 
     runCmd java -javaagent:multithread-context-0.9.0-SNAPSHOT.jar \
-    -cp $classpath:multithread-context-0.9.0-SNAPSHOT.jar:test-classes/ \
+    -Xbootclasspath/a:$classpath:multithread-context-0.9.0-SNAPSHOT.jar:test-classes/   \
     com.oldratlee.mtc.threadpool.agent.AgentDemo
 }
