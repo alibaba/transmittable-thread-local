59270950ea2662678b160bac71432e841659b935
Jerry Lee <oldratlee@gmail.com>
2015-06-16 23:29:24 +0800

    improve javadoc

---
+++ src/main/java/com/alibaba/mtc/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * MTC API.
+ *
+ * @author Jerry Lee (oldratlee at gmail dot com)
+ */
+package com.alibaba.mtc;
---
+++ src/main/java/com/alibaba/mtc/threadpool/agent/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * MTC Agent.
+ *
+ * @author Jerry Lee (oldratlee at gmail dot com)
+ */
+package com.alibaba.mtc.threadpool.agent;
