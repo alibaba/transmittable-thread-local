67152ad736b72ec925757b8ce62377aa408ed9cc
Jerry Lee <oldratlee@gmail.com>
2017-09-20 10:50:20 +0800

    add fork join pool demo

---
+++ src/test/java/com/alibaba/ttl/demo/forkjoinpool/Demo.java
@@ -0,0 +1,67 @@
+package com.alibaba.ttl.demo.forkjoinpool;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.LongStream;
+
+import static org.junit.Assert.fail;
+
+/**
+ * ForkJoinPool use demo.
+ *
+ * @author Jerry Lee (oldratlee at gmail dot com)
+ */
+public class Demo {
+    public static void main(String[] args) throws Exception {
+        final ForkJoinPool pool = new ForkJoinPool();
+
+        long[] numbers = LongStream.rangeClosed(1, 100000).toArray();
+        final Long result = pool.invoke(new SumTask(numbers));
+
+        System.out.println(result); // result is 5000050000
+
+        pool.shutdown();
+        pool.awaitTermination(100, TimeUnit.MILLISECONDS);
+        if (!pool.isTerminated()) fail("Fail to shutdown thread pool");
+    }
+}
+
+class SumTask extends RecursiveTask<Long> {
+    private final long[] numbers;
+    private final int from;
+    private final int to;
+
+    SumTask(long[] numbers) {
+        this(numbers, 0, numbers.length - 1);
+    }
+
+    private SumTask(long[] numbers, int from, int to) {
+        this.numbers = numbers;
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    protected Long compute() {
+        final int delta = to - from;
+        if (delta < 16) {
+            // compute directly
+            long total = 0;
+            for (int i = from; i <= to; i++) {
+                total += numbers[i];
+            }
+            return total;
+        } else {
+            // split task
+            final int middle = from + delta / 2;
+
+            SumTask taskLeft = new SumTask(numbers, from, middle);
+            SumTask taskRight = new SumTask(numbers, middle + 1, to);
+
+            taskLeft.fork();
+            taskRight.fork();
+            return taskLeft.join() + taskRight.join();
+        }
+    }
+}
---
+++ src/test/java/com/alibaba/ttl/forkjoin/ForkJoinPoolTest.java
@@ -0,0 +1,81 @@
+package com.alibaba.ttl.forkjoin;
+
+import org.junit.AfterClass;
+import org.junit.Test;
+
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.RecursiveTask;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.LongStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+
+public class ForkJoinPoolTest {
+    private static final ForkJoinPool pool = new ForkJoinPool();
+
+    @AfterClass
+    public static void afterClass() throws Exception {
+        pool.shutdown();
+        pool.awaitTermination(100, TimeUnit.MILLISECONDS);
+        if (!pool.isTerminated()) fail("Fail to shutdown thread pool");
+    }
+
+    @Test
+    public void test_sameTaskDirectReturn_onlyExec1Time_ifHaveRun() throws Exception {
+        long[] numbers = LongStream.rangeClosed(1, 1000).toArray();
+        final SumTask sumTask = new SumTask(numbers);
+
+        // same task instance run 10 times
+        for (int i = 0; i < 10; i++) {
+            assertEquals(500500L, pool.invoke(sumTask).longValue());
+        }
+
+        assertEquals(1, sumTask.execCounter.get());
+    }
+}
+
+
+class SumTask extends RecursiveTask<Long> {
+    private final long[] numbers;
+    private final int from;
+    private final int to;
+
+    AtomicInteger execCounter = new AtomicInteger(0);
+
+    SumTask(long[] numbers) {
+        this(numbers, 0, numbers.length - 1);
+    }
+
+    private SumTask(long[] numbers, int from, int to) {
+        this.numbers = numbers;
+        this.from = from;
+        this.to = to;
+    }
+
+    @Override
+    protected Long compute() {
+        execCounter.incrementAndGet();
+
+        final int delta = to - from;
+        if (delta < 16) {
+            // compute directly
+            long total = 0;
+            for (int i = from; i <= to; i++) {
+                total += numbers[i];
+            }
+            return total;
+        } else {
+            // split task
+            final int middle = from + delta / 2;
+
+            SumTask taskLeft = new SumTask(numbers, from, middle);
+            SumTask taskRight = new SumTask(numbers, middle + 1, to);
+
+            taskLeft.fork();
+            taskRight.fork();
+            return taskLeft.join() + taskRight.join();
+        }
+    }
+}
