aba6f03fdebc558bb353101d985cf7855934e3e5
Jerry Lee <oldratlee@gmail.com>
2018-06-11 13:12:33 +0800

    rename: pom4intellij.xml -> pom4ide.xml

---
+++ scritps/gen-pom4ide.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cd "$(dirname "$(readlink -f "$0")")"/..
+
+sed '
+    s#<source>1\.6</source>#<source>1.8</source>#
+    s#<target>1\.6</target>#<target>1.8</target>#
+' pom.xml > pom4ide.xml
