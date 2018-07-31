167b10b3ee3a189f36a69cd2410ce84c622a0a1d
Jerry Lee <oldratlee@gmail.com>
2018-07-31 14:56:17 +0800

    add Boot-Class-Path manifest attributes
    - simplify boot class path setting
    - remove -Xbootclasspath setting in CI
    
    more info see
    - the agent official doc:
    https://docs.oracle.com/javase/8/docs/api/java/lang/instrument/package-summary.html#package.description
    - #99

--- scripts/run-agent-demo.sh
+++ scripts/run-agent-demo.sh
@@ -4,5 +4,5 @@ cd "$(dirname "$(readlink -f "$0")")"
 source ./common.sh
 
 runCmd "${JAVA_CMD[@]}" -cp "$(getClasspathWithoutTtlJar)" \
-    "-Xbootclasspath/a:$(getTtlJarPath)" "-javaagent:$(getTtlJarPath)=ttl.agent.logger:STDOUT" \
+    "-javaagent:$(getTtlJarPath)=ttl.agent.logger:STDOUT" \
     com.alibaba.demo.agent.AgentDemo
