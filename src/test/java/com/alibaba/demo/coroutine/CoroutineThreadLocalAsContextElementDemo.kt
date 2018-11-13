660cbbf49c1317f26c02bf9d0f4d5d246f8c30de
Jerry Lee <oldratlee@gmail.com>
2018-11-13 13:34:46 +0800

    add CoroutineThreadLocalAsContextElementDemo

---
+++ src/test/java/com/alibaba/demo/coroutine/CoroutineThreadLocalAsContextElementDemo.kt
@@ -0,0 +1,20 @@
+import kotlinx.coroutines.*
+
+val threadLocal = ThreadLocal<String?>() // declare thread-local variable
+
+/**
+ * [Thread-local data - Coroutine Context and Dispatchers - Kotlin Programming Language](https://kotlinlang.org/docs/reference/coroutines/coroutine-context-and-dispatchers.html#thread-local-data)
+ */
+fun main() = runBlocking<Unit> {
+    threadLocal.set("main")
+    println("Pre-main, current thread: ${Thread.currentThread()}, thread local value: ${threadLocal.get()}")
+
+    val job = launch(Dispatchers.Default + threadLocal.asContextElement(value = "launch")) {
+        println("Launch start, current thread: ${Thread.currentThread()}, thread local value: ${threadLocal.get()}")
+        yield()
+        println("After yield, current thread: ${Thread.currentThread()}, thread local value: ${threadLocal.get()}")
+    }
+    job.join()
+
+    println("Post-main, current thread: ${Thread.currentThread()}, thread local value: ${threadLocal.get()}")
+}
