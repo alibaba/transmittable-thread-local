7ea7482c544f7b51d888d4d993578efdc59a4747
Jerry Lee <oldratlee@gmail.com>
2019-06-06 13:22:44 +0800

    improve gen-pom4ide.sh: output diff

--- scripts/gen-pom4ide.sh
+++ scripts/gen-pom4ide.sh
@@ -6,3 +6,8 @@ sed '
     s#<source>1\.6</source>#<source>1.8</source>#
     s#<target>1\.6</target>#<target>1.8</target>#
 ' pom.xml > pom4ide.xml
+
+echo "diff between pom and pom4ide.xml:"
+echo
+
+diff pom.xml pom4ide.xml
