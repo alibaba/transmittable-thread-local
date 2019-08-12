cac6d9e1349fe0d36accb4d5ac46fa234a8f97d3
Jerry Lee <oldratlee@gmail.com>
2019-08-12 13:23:18 +0800

    add checke-japi-compliance.sh

---
+++ scripts/checke-japi-compliance.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Java API Compliance Checker (JAPICC),
+# a tool for checking backward binary and source-level compatibility of a Java library API.
+#   https://github.com/lvc/japi-compliance-checker
+
+cd "$(dirname "$(readlink -f "$0")")"
+
+export TTL_CI_TEST_MODE=true
+source ./common.sh skipClean
+
+set -u
+
+ttl_jar_path="$(readlink -f "$(getTtlJarPath)")"
+JCC="$(readlink -f "$(which japi-compliance-checker.pl)")"
+
+work_dir="target/japi-compliance-checker"
+mkdir -p $work_dir
+cd $work_dir
+rm -rf compat_reports
+
+for base_version in 2.5.0 2.6.0 2.7.0 2.10.2; do
+    url="https://repo1.maven.org/maven2/com/alibaba/transmittable-thread-local/$base_version/transmittable-thread-local-$base_version.jar"
+    base_jar="transmittable-thread-local-$base_version.jar"
+    if [ ! -f "$base_jar" ]; then
+        wget "$url"
+    fi
+
+    "$JCC" -show-packages -check-annotations -skip-internal-packages '\.(javassist|utils?|internal)\.?' \
+        "$base_jar" \
+        "$ttl_jar_path" || true
+done
