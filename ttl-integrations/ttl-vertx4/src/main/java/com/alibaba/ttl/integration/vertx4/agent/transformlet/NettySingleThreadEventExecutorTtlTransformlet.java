7c56f93ae0820c4472c92d69839f7cbe14cf5aac
kun <305809299@qq.com>
2021-01-21 18:22:33 +0800

    + add vert.x 4 integration

---
+++ ttl-integrations/ttl-vertx4/src/main/java/com/alibaba/ttl/integration/vertx4/agent/transformlet/NettySingleThreadEventExecutorTtlTransformlet.java
@@ -0,0 +1,29 @@
+package com.alibaba.ttl.integration.vertx4.agent.transformlet;
+
+import com.alibaba.ttl.threadpool.agent.transformlet.helper.AbstractExecutorTtlTransformlet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * {@link com.alibaba.ttl.threadpool.agent.transformlet.TtlTransformlet}
+ * for {@link io.netty.util.concurrent.SingleThreadEventExecutor}.
+ *
+ * @see io.netty.util.concurrent.SingleThreadEventExecutor
+ * @see io.vertx.core.eventbus.EventBus
+ * @see io.vertx.core.impl.EventLoopContext
+ * @see io.vertx.core.eventbus.Message
+ */
+public final class NettySingleThreadEventExecutorTtlTransformlet extends AbstractExecutorTtlTransformlet {
+    private static final Set<String> EXECUTOR_CLASS_NAMES = new HashSet<String>();
+
+    static {
+        EXECUTOR_CLASS_NAMES.add("io.netty.util.concurrent.SingleThreadEventExecutor");
+    }
+
+    private static final String THREAD_FACTORY_CLASS_NAME = "java.util.concurrent.ThreadFactory";
+
+    public NettySingleThreadEventExecutorTtlTransformlet(boolean disableInheritableForThreadPool) {
+        super(EXECUTOR_CLASS_NAMES, disableInheritableForThreadPool);
+    }
+}
