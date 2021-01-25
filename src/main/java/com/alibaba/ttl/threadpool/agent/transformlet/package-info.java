1a7dbdd54406ff2435a08e42f887680cb8600073
Jerry Lee <oldratlee@gmail.com>
2021-01-25 14:39:18 +0800

    + add AbstractExecutorTtlTransformlet

--- src/main/java/com/alibaba/ttl/threadpool/agent/transformlet/package-info.java
+++ src/main/java/com/alibaba/ttl/threadpool/agent/transformlet/package-info.java
@@ -1,5 +1,6 @@
 /**
- * TTL {@code Transformlet} API and built-in internal implementations.
+ * TTL {@code Transformlet} API and
+ * built-in internal implementations(in the package {@link com.alibaba.ttl.threadpool.agent.transformlet.internal}).
  *
  * @author Jerry Lee (oldratlee at gmail dot com)
  * @see com.alibaba.ttl.threadpool.agent.transformlet.TtlTransformlet
