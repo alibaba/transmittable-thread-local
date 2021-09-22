69c9a7ca72336263e3dc857a012906afc1bda11d
removedporn <86824510+removedporn@users.noreply.github.com>
2021-09-22 15:06:21 +0800

    Delete ttl-integrations directory

--- ttl-integrations/sample-ttl-agent-extension-transformlet/src/main/java/com/alibaba/ttl/agent/extension_transformlet/sample/biz/ToBeTransformedClass.java
+++
@@ -1,7 +0,0 @@
-package com.alibaba.ttl.agent.extension_transformlet.sample.biz;
-
-public class ToBeTransformedClass {
-    public int toBeTransformedMethod(int input) {
-        return input;
-    }
-}
