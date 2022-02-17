c54fb0efaa9f310c9f486bda05316956de3d266f
Jerry Lee <oldratlee@gmail.com>
2022-02-17 23:54:50 +0800

    chore: improve `clear-appveyor-cache.ps1`

--- scripts/clear-appveyor-cache.ps1
+++ scripts/clear-appveyor-cache.ps1
@@ -1,7 +1,9 @@
 #!/usr/bin/env pwsh
 
+$appveyor_project="oldratlee/transmittable-thread-local"
+
 # https://stackoverflow.com/questions/24649019/how-to-use-confirm-in-powershell
-$confirmation = Read-Host "Are you Sure You Want To Clear cache of appveyor project oldratlee/transmittable-thread-local"
+$confirmation = Read-Host "Are you Sure You Want To Clear cache of appveyor project $appveyor_project [y/N]"
 if ($confirmation -ne 'y') {
     Write-Output "do nothing and exit"
     exit
@@ -28,4 +30,4 @@ if ($confirmation -ne 'y') {
 $headers = @{}
 $headers['Authorization'] = "Bearer $appveyor_token"
 $headers["Content-type"] = "application/json"
-Invoke-RestMethod -Uri 'https://ci.appveyor.com/api/projects/oldratlee/transmittable-thread-local/buildcache' -Headers $headers -Method Delete
+Invoke-RestMethod -Uri "https://ci.appveyor.com/api/projects/$appveyor_project/buildcache" -Headers $headers -Method Delete
