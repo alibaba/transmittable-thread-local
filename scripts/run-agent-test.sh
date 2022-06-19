6d22bd598f28cfa4b195dd01fba191d5e9688051
Jerry Lee <oldratlee@gmail.com>
2022-06-19 18:02:20 +0800

    chore(ci): use `maven test` instead of hand-writing `junit` command in script `run-junit.sh`/`run-agent-test.sh`

--- scripts/run-agent-test.sh
+++ scripts/run-agent-test.sh
@@ -10,19 +10,6 @@ source ./ttl_build.sh
 # do heavy operation first, decrease mvn operation count.
 cu::log_then_run mvnBuildJar
 
-cu::blue_echo 'Run unit test under ttl agent, include check for ExecutorService, ForkJoinPool'
-cu::log_then_run "${JAVA_CMD[@]}" -cp "$(getClasspathWithoutTtlJar)" \
-    "-javaagent:$(getTtlJarPath)=ttl.agent.logger:STDOUT" \
-    -Drun-ttl-test-under-agent=true \
-    org.junit.runner.JUnitCore $(getJUnitTestCases | grep -vE '\.TransmittableThreadLocal_withInit_Null_Test$')
-
-cu::blue_echo 'Run unit test under ttl agent, and turn on the disable inheritable for thread pool enhancement'
-cu::log_then_run "${JAVA_CMD[@]}" -cp "$(getClasspathWithoutTtlJar)" \
-    "-javaagent:$(getTtlJarPath)=ttl.agent.logger:STDOUT,ttl.agent.disable.inheritable.for.thread.pool:true" \
-    -Drun-ttl-test-under-agent=true \
-    -Drun-ttl-test-under-agent-with-disable-inheritable=true \
-    org.junit.runner.JUnitCore $(getJUnitTestCases | grep -vE '\.TransmittableThreadLocal_withInit_Null_Test$')
-
 cu::blue_echo 'Run agent check for Timer/TimerTask, default "ttl.agent.enable.timer.task"'
 cu::log_then_run "${JAVA_CMD[@]}" -cp "$(getClasspathWithoutTtlJar)" \
     "-javaagent:$(getTtlJarPath)=ttl.agent.logger:STDOUT" \
