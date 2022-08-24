70ffed85c3822f4cff0835bbd64042bff991c162
Jerry Lee <oldratlee@gmail.com>
2022-08-24 14:04:05 +0800

    refactor/docs: reorder methods of `Wrappers`; improve javadoc

--- ttl-core/src/main/java/com/alibaba/ttl3/spi/package-info.java
+++ ttl-core/src/main/java/com/alibaba/ttl3/spi/package-info.java
@@ -4,6 +4,7 @@
  *
  * @author Jerry Lee (oldratlee at gmail dot com)
  * @see com.alibaba.ttl3.spi.TtlEnhanced
+ * @see com.alibaba.ttl3.spi.TtlWrapper
  * @see com.alibaba.ttl3.spi.TtlAttachments
  */
 package com.alibaba.ttl3.spi;
