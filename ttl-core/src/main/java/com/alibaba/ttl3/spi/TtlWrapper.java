3c400f55758db55ef7fc99ee3ae70f57c1a2c896
Jerry Lee <oldratlee@gmail.com>
2023-12-17 01:05:59 +0800

    docs: improve javadoc of `TtlWrapper`/`TtlWrappers` 📚

--- ttl-core/src/main/java/com/alibaba/ttl3/spi/TtlWrapper.java
+++ ttl-core/src/main/java/com/alibaba/ttl3/spi/TtlWrapper.java
@@ -1,33 +1,35 @@
 package com.alibaba.ttl3.spi;
 
-import com.alibaba.ttl3.executor.TtlExecutors;
 import edu.umd.cs.findbugs.annotations.NonNull;
-
-import java.util.concurrent.ExecutorService;
 
 /**
  * Ttl Wrapper interface.
  * <p>
  * Used to mark wrapper types, for example:
  * <ul>
- *     <li>{@link com.alibaba.ttl3.TtlCallable TtlCallable}
- *     <li>{@code TtlExecutors} created by util method
- *         {@link TtlExecutors#getTtlExecutorService(ExecutorService)} getTtlExecutorService}
- *     <li>{@code DisableInheritableThreadFactory} created by util method
- *         {@link TtlExecutors#getDisableInheritableThreadFactory(java.util.concurrent.ThreadFactory) getDisableInheritableThreadFactory}
+ *     <li>{@link com.alibaba.ttl3.TtlRunnable TtlRunnable}s, {@link com.alibaba.ttl3.TtlCallable TtlCallable}s
+ *     <li>{@code TtlWrappers} created by util methods {@code wrap*()} of class
+ *         {@link com.alibaba.ttl3.TtlWrappers TtlWrappers},
+ *         e.g. {@link com.alibaba.ttl3.TtlWrappers#wrapSupplier(java.util.function.Supplier) wrapSupplier}
+ *     <li>{@code TtlExecutors} created by util methods {@code get*()} of class
+ *         {@link com.alibaba.ttl3.executor.TtlExecutors TtlExecutors},
+ *         e.g. {@link com.alibaba.ttl3.executor.TtlExecutors#getTtlExecutorService(java.util.concurrent.ExecutorService) getTtlExecutorService}
+ *     <li>{@code DisableInheritableThreadFactories} created by util method
+ *         {@link com.alibaba.ttl3.executor.TtlExecutors#getDisableInheritableThreadFactory(java.util.concurrent.ThreadFactory) getDisableInheritableThreadFactory}
  * </ul>
  *
  * @author Jerry Lee (oldratlee at gmail dot com)
  * @see com.alibaba.ttl3.TtlWrappers#unwrap
- * @see com.alibaba.ttl3.TtlCallable
  * @see com.alibaba.ttl3.TtlRunnable
+ * @see com.alibaba.ttl3.TtlCallable
+ * @see com.alibaba.ttl3.TtlWrappers
  * @see com.alibaba.ttl3.executor.TtlExecutors
  */
 public interface TtlWrapper<T> extends TtlEnhanced {
     /**
      * unwrap {@link TtlWrapper} to the original/underneath one.
      *
-     * @see com.alibaba.ttl3.TtlWrappers#unwrap(Object)
+     * @see com.alibaba.ttl3.TtlWrappers#unwrap
      */
     @NonNull
     T unwrap();
