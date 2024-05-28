b03a1b29807dc4c8728914b209065adb8b22726d
chengpu <raozihao.rzh@alibaba-inc.com>
2024-05-28 14:14:04 +0800

    Polish the codes

--- ttl-agent/src/main/java/com/alibaba/ttl3/agent/TtlTransformer.java
+++ ttl-agent/src/main/java/com/alibaba/ttl3/agent/TtlTransformer.java
@@ -1,6 +1,7 @@
 package com.alibaba.ttl3.agent;
 
 import static com.alibaba.ttl3.agent.transformlet.helper.TtlTransformletHelper.isClassUnderPackage;
+import static com.alibaba.ttl3.agent.transformlet.helper.TtlTransformletHelper.isUnnecessaryPackage;
 
 import com.alibaba.ttl3.agent.logging.Logger;
 import com.alibaba.ttl3.agent.transformlet.ClassInfo;
@@ -11,10 +12,7 @@ import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import java.lang.instrument.ClassFileTransformer;
 import java.security.ProtectionDomain;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 /**
  * TTL {@link ClassFileTransformer} of Java Agent
@@ -65,7 +63,7 @@ public class TtlTransformer implements ClassFileTransformer {
             final ClassInfo classInfo = new ClassInfo(classFile, classFileBuffer, loader);
             if (isClassUnderPackage(classInfo.getClassName(), "com.alibaba.ttl")) return NO_TRANSFORM;
             if (isClassUnderPackage(classInfo.getClassName(), "java.lang")) return NO_TRANSFORM;
-            if (isExcludedPackages(classInfo.getClassName())) return NO_TRANSFORM;
+            if (isUnnecessaryPackage(classInfo.getClassName())) return NO_TRANSFORM;
 
             if (logClassTransform)
                 logger.info("[TtlTransformer] transforming " + classInfo.getClassName()
@@ -98,16 +96,5 @@ public class TtlTransformer implements ClassFileTransformer {
         }
 
         return NO_TRANSFORM;
-    }
-
-    private boolean isExcludedPackages(String packageName) {
-        // -DexcludePackages=com.xxx, org.xxx
-        String excludePackages = System.getProperty("excludePackages");
-        if (excludePackages == null) {
-            return false;
-        }
-        Set<String> packageSet = Arrays.stream(excludePackages.split(","))
-            .collect(Collectors.toSet());
-        return packageSet.contains(packageName);
     }
 }
